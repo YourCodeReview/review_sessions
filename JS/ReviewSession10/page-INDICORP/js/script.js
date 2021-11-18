d1084fd3593040a743a784edca00d195587cec99
YourCodeReview <yourcodereview@gmail.com>
2021-11-18 08:56:32 +0100

    page-INDICORP from Albert-webJS  for review

---
+++ JS/ReviewSession10/page-INDICORP/js/script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+// let burgerBtn = document.querySelector(".icon-burger__top");
+
+// burgerBtn.onclick = function () {
+//   this.classList.toggle("icon-burger__top--active");
+//   console.log("say hi!");
+// };
